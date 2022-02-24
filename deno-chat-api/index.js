b124bd3e1a8391cb60b3ece9cd03cee642ba32d3
VicenteFleitas <vicente1fleitas1@gmail.com>
2022-02-23 21:40:13 -0300

    second commit test

--- deno-chat-api/index.js
+++ deno-chat-api/index.js
@@ -1,5 +1,5 @@
 addEventListener("fetch", e => {
-	const res = new Response("Hello world!", {
+	const res = new Response("Hello my world!", {
 		headers: { "content-type": "text/plain" }
 	})
 	e.respondWith(res);
