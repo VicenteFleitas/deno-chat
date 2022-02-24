9e842e672aa493c6567a6b1aed6d37f31300b0ec
VicenteFleitas <vicente1fleitas1@gmail.com>
2022-02-23 23:23:46 -0300

    update 1

--- deno-chat-api/index.js
+++ deno-chat-api/index.js
@@ -1,9 +1,23 @@
-import { Application } from "https://deno.land/x/oak@v9.0.0/mod.ts"; //  oak@v7.1.0
+import { Application, Router } from "https://deno.land/x/oak@v9.0.0/mod.ts"; //  oak@v7.1.0
+
+const messages = [];
+
+const router = new Router();
+router
+	.get("/", (context) => {
+		context.response.body = "Chat server!";
+	})
+	.get("/messages", (context) => {
+		context.response.body = messages;
+	})
+	.post("/messages", async (context) => {
+		const message = await context.request.body().value;
+		messages.push(message);
+		context.response.body = messages;
+	});
 
 const app = new Application();
-
-app.use((ctx) => {
-	ctx.response.body = "Hello world! again.";
-});
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 addEventListener("fetch", app.fetchEventHandler());
