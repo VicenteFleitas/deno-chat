348add5a3af9a094936d678c17594b4204f7b815
VicenteFleitas <vicente1fleitas1@gmail.com>
2022-02-23 21:49:36 -0300

    use oak

--- deno-chat-api/index.js
+++ deno-chat-api/index.js
@@ -1,6 +1,9 @@
-addEventListener("fetch", e => {
-	const res = new Response("Hello my world!", {
-		headers: { "content-type": "text/plain" }
-	})
-	e.respondWith(res);
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+
+app.use(ctx => {
+	ctx.response.body = "Hello world!, again.";
 })
+
+addEventListener("fetch", app.fetchEvnetHandler());
