1bbe6b955838b3d58c3b43d902371dd9a369ee02
VicenteFleitas <vicente1fleitas1@gmail.com>
2022-02-23 22:11:42 -0300

    f4

--- deno-chat-api/index.js
+++ deno-chat-api/index.js
@@ -2,8 +2,8 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 
 const app = new Application();
 
-app.use(async (ctx) => {
-	await ctx.response.body = "Hello world! again.";
+app.use((ctx) => {
+	ctx.response.body = "Hello world! again.";
 });
 
 addEventListener("fetch", app.fetchEventHandler());
